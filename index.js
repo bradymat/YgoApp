d739e7190c8373e139592855621dd056092811c2
bradymat <bradymat@Bradys-MacBook-Air.local>
2016-09-28 15:18:36 +1300

    testing git linked

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+
+render(<App name='Ygo App' />, document.querySelector('main'))
+console.log('welcome to Ygo App')
---
+++ test/index.js
@@ -0,0 +1,6 @@
+var test = require('tape')
+
+test('testing Ygo App', function (t) {
+  t.ok(true)
+  t.end()
+})
